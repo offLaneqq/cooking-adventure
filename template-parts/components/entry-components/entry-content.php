6cd0876cfe3b6a16035f57428b45976d1732dace
Dmytro <srk90512@gmail.com>
2024-03-08 15:19:11 +0200

    add template for each post

---
+++ template-parts/components/entry-components/entry-content.php
@@ -0,0 +1,35 @@
+<?php
+
+// Template for entry content
+
+?>
+
+<div class="entry-content">
+    <?php
+    if (is_single()) {
+        the_content(
+            sprintf(
+                wp_kses(
+                    __('Continue reading %s <span class="meta-nav">rarr;</span>', 'cook-adv'),
+                    [
+                        "span" => [
+                            "class" => []
+                        ]
+                    ]
+                ),
+                the_title('<span class="screen-reader-text">"', '"</span>', false)
+            )
+        );
+        // Pagination in single post(pages breaks in wp)
+        wp_link_pages(
+            [
+                'before' => '<div class="page-links">' . esc_html__('Pages: ', 'cook'),
+                'after' => '</div>'
+            ]
+        );
+    } else {
+        cook_the_excerpt(350);
+
+    }
+    ?>
+</div>
---
+++ template-parts/components/entry-components/entry-meta.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Post meta information
+ */
+
+?>
+
+<div class="entry-meta mb-3">
+    <?php
+
+    cook_posted_on(); 
+    cook_posted_by();
+
+    ?>
+</div>
