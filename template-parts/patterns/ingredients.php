a207042faa7e02729622bffeb50a8596def07d01
Dmytro <srk90512@gmail.com>
2024-03-20 00:19:55 +0200

    pattern for list of ingredients

---
+++ template-parts/patterns/ingredients.php
@@ -0,0 +1,16 @@
+<?php
+
+// Content for pattern List of Ingredients
+
+?>
+
+<!-- wp:paragraph -->
+<p><strong>Ingredients:</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+    <li></li>
+    <!-- /wp:list-item -->
+</ul>
+<!-- /wp:list -->
