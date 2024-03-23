3f48b390f5ad6564c2c0d33a239e2c7c49eb0748
Dmytro <srk90512@gmail.com>
2024-03-23 17:15:19 +0200

    update footer and About Us

--- footer.php
+++ footer.php
@@ -6,19 +6,46 @@
 
 ?>
 </div>
-<footer>
-    <h3>Footer</h3>
-    <?php
-    if (is_active_sidebar('sidebar-2')) {
-    ?>
-        <aside>
-            <?php dynamic_sidebar('sidebar-2') ?>
-        </aside>
-    <?php
-    }
-    ?>
+<footer id="site-footer" class="bg-light p-4">
+    <div class="container color-gray">
+        <div class="row">
+            <section class="col-lg-4 col-md-6 col-sm-12">
+                <h1><strong><a href="http://cooking-theme.local/" class="footer-site-logo d-block mb-4">Cooking Adventure</a></strong></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quasi perferendis ratione perspiciatis accusantium.</p>
+            </section>
+            <section class="col-lg-4 col-md-6 col-sm-12">
+                <?php
+                if (is_active_sidebar('sidebar-2')) {
+                ?>
+                    <aside>
+                        <?php dynamic_sidebar('sidebar-2') ?>
+                    </aside>
+                <?php
+                }
+                ?>
+            </section>
+            <section class="col-lg-4 col-md-6 col-sm-12">
+                <ul class="d-flex">
+                    <li class="list-unstyled"><a href="https://www.linkedin.com/" title="linkedin">
+                            <svg width="48">
+                                <use href="#icon-linkedin"></use>
+                            </svg>
+                        </a></li>
+                </ul>
+            </section>
+        </div>
+        <div class="row ">
+            <div class="col-12 text-center">
+                <div class="copyright mt-5">
+                    <p><small>© 2023-2024 All Rights Reserved.</small></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
-<?php wp_footer(); ?>
+<?php
+get_template_part('template-parts/content', 'svgs');
+wp_footer(); ?>
 </div>
 </body>
 
--- page-about-us.php
+++ page-about-us.php
@@ -4,7 +4,14 @@ get_header();
 
 ?>
 
-qwe
+<div class="container text-center mt-5">
+    <h1>Our Story</h1>
+    <p>This is my first website project written in php using JavaScript, SCSS and WordPress.
+        I decided to try to write something related to recipes, because I
+        thought it would be relevant to both the older generation and the younger generation,
+        because everyone is equal in front of cooking.
+    </p>
+</div>
 
 <?php
 
